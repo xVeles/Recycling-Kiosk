b8a6edc72941fd8dd452aad1daf46f4c9830ae85
Shannon Kuah <shankuah62@gmail.com>
2020-04-14 14:27:45 +1200

    Merged map

--- WebAppDeploy/js/script.js
+++ WebAppDeploy/js/script.js
@@ -1,50 +1,3 @@
-<<<<<<< Updated upstream
-//Buttons
-let startBtn = document.getElementById("startBtn");
-let accountBtn = document.getElementById("accountBtn");
-let rckBtn = document.getElementById("rckBtn");
-let pwdChngBtn = document.getElementById("pwdChngBtn");
-
-//area for page
-let mainHeader = document.getElementById("mainHeader");
-let mainContent = document.getElementById("mainContent");
-
-//Tabs
-let accountContent = document.getElementById("account-tab");
-let startContent = document.getElementById("start-tab");
-
-
-
-//button eventlisteners
-startBtn.addEventListener("click", function() {
-    alert("I have been clicked");
-});
-
-accountBtn.addEventListener("click", function() {
-    loadAccount();
-});
-
-rckBtn.addEventListener("click", function() {
-    loadRCK();
-});
-
-function loadAccount() 
-{
-    disableAllTabs();
-    accountContent.setAttribute("class", "container h-100 active");
-}
-
-function loadRCK() {
-    disableAllTabs();
-    startContent.setAttribute("class", "row h-100 active");
-}
-
-function disableAllTabs()
-{
-    startContent.setAttribute("class", "row h-100 hidden");
-    accountContent.setAttribute("class", "container h-100 hidden");
-}
-=======
 // Port needs to be the same as the ISS host port
 let _port = 8989;
 
@@ -148,5 +101,3 @@ function showPosition(position)
     });
     loading.setAttribute("class", "hidden");
 }
-
->>>>>>> Stashed changes
