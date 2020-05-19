81ff687b5b3ba58d3a5cc2a8187ac170308c6868
Matthew Smith <37426509+MatthewJamesS@users.noreply.github.com>
2020-05-20 11:31:10 +1200

    Revert "Merge branch 'master' into Web-Pages-Integrations"
    This reverts commit cc8859b9c60ffab2705d8a5d5d78acc051df7f13, reversing
    changes made to 84e4e56a9ddf7fecc0fc67b99bea573b0adc538b.

--- Recycling Kiosk/Objects/ShopItem.cs
+++ Recycling Kiosk/Objects/ShopItem.cs
@@ -11,9 +11,7 @@ namespace Recycling_Kiosk.Objects
         public string Name { get; set; }
         public string Description { get; set; }
         public int Stock { get; set; }
-        public int CategoryID { get; set; }
         public double Price { get; set; }
         public string ShopImg { get; set; }
-        public string Size { get; set; }
     }
 }
